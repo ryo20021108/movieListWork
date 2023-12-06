54ee84d803b8184685a31962e9aa1d1f1ec2193f
ryo20021108 <pig20021108@gmail.com>
2023-12-06 23:32:39 +0800

    mode switch

--- index.js
+++ index.js
@@ -10,21 +10,106 @@ const VIEW_MODE = {
 }
 
 
+//-----
+const dataPanel = document.querySelector('#data-panel')
+
+
+//-----MODEL-----//
 const model = {
-  movies: []
-}
+  movies: [],
 
-
-const view = {
-}
-
-const controller = {
-  generateMoviesList() {
-    axios.get(INDEX_URL).then(response => {
-      model.movies.push(...response.data.results)
-      console.log(model.movies)
+  AXIOS_API() {
+    return axios.get(INDEX_URL).then(response => {
+      this.movies.push(...response.data.results)
     }).catch((err) => console.log(err))
+  },
+
+
+}
+
+//-----VIEW-----//
+const view = {
+  modeSwitch: document.querySelector('#mode-switch'),
+
+  currentModeCheck() {
+    this.modeSwitch.addEventListener('click', (event) => {
+      if (event.target.classList.contains('fa-solid')) {
+        if (event.target.classList.contains('fa-grip')) {
+          controller.currentMode = VIEW_MODE.cardsMode;
+        } else if (event.target.classList.contains('fa-bars')) {
+          controller.currentMode = VIEW_MODE.listMode;
+        }
+        this.renderMovies(model.movies);
+        console.log(controller.currentMode);
+      }
+    });
+  },
+
+  renderMovies(data) {
+    let rawHTML = ''
+    switch (controller.currentMode) {
+
+      case VIEW_MODE.cardsMode:
+        data.forEach(item => {
+          rawHTML += `
+            <div class="col-sm-3">
+              <div class="mb-3">
+                <div class="card">
+                  <img src="${POSTER_URL}${item.image}" class="card-img-top"
+                    alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title" >${item.title}</h5>
+                  </div>
+                  <div class="card-footer d-flex justify-content-end">
+                    <button href="#" class="btn btn-primary me-1 btn-show-movie" data-bs-toggle="modal"
+                      data-bs-target="#movie-more">More</button>
+                    <button href="#" class="btn btn-danger btn-favorite">+</button>
+                  </div>
+                </div>
+              </div>
+            </div>`
+        });
+        break;
+
+      case VIEW_MODE.listMode:
+        rawHTML = `<ul class="list-group" id="list-group">`
+        data.forEach(item => {
+          rawHTML += `
+          <li class="list-group-item d-flex justify-content-between ">
+            <img src="${POSTER_URL}${item.image}" alt="">
+            <div>
+              <h5>${item.title}</h5>
+            </div>
+            <div> 
+              <button href="#" class="btn btn-primary me-1 btn-show-movie" data-bs-toggle="modal"
+                data-bs-target="#movie-more">More</button>
+              <button href="#" class="btn btn-danger btn-favorite">+</button>
+            </div>
+          </li>
+          `
+        });
+        rawHTML += `</ul>`
+        break;
+    }
+    dataPanel.innerHTML = rawHTML
   }
 }
 
+//-----CONTROLLER-----//
+const controller = {
+  currentMode: VIEW_MODE.cardsMode,
+
+  generateMoviesList() {
+    model.AXIOS_API()
+      .then(() => {
+        view.renderMovies(model.movies);
+      })
+      .catch(err => console.log(err));
+  }
+
+
+};
+
 controller.generateMoviesList();
+view.currentModeCheck();
+
